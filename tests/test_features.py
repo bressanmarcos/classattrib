4adbd157b46a677cde22b940e7b5b02dd9ea59af
Marcos Bressan <bressan@dee.ufc.br>
2023-11-11 22:42:05 -0300

    test: Tests for static and no_default

--- tests/test_features.py
+++ tests/test_features.py
@@ -7,34 +7,46 @@ from classattrib import DynamicAttribute, DynamicClass, SetClassAttribute
 def dynamic_class():
     @DynamicClass
     class MyDynamicClass:
-        common = "shared"
-        local = DynamicAttribute(None)
+        static = "shared"
+        dynamic = DynamicAttribute(None)
+        dynamic_no_default = DynamicAttribute()
 
     return MyDynamicClass
 
 
+def test_static_attribute_still_works_as_usual(dynamic_class):
+    assert dynamic_class.static == "shared"
+
+
 def test_access_default_value(dynamic_class):
-    assert dynamic_class.local == None
+    assert dynamic_class.dynamic == None
+
+
+def test_access_no_default_value(dynamic_class):
+    with pytest.raises(AttributeError):
+        dynamic_class.dynamic_no_default
 
 
 def test_class_attribute_patch_and_rollback(dynamic_class):
-    assert dynamic_class.local == None
+    assert dynamic_class.dynamic == None
 
-    with SetClassAttribute(dynamic_class, local=42):
-        assert dynamic_class.local == 42
+    with SetClassAttribute(dynamic_class, dynamic=42):
+        assert dynamic_class.dynamic == 42
 
-    assert dynamic_class.local == None
+    assert dynamic_class.dynamic == None
 
 
 def test_class_attribute_nested_patching(dynamic_class):
-    assert dynamic_class.local == None
+    assert dynamic_class.dynamic == None
 
-    with SetClassAttribute(dynamic_class, local=42):
-        assert dynamic_class.local == 42
+    with SetClassAttribute(dynamic_class, dynamic=42):
+        assert dynamic_class.dynamic == 42
 
-        with SetClassAttribute(dynamic_class, local=99):
-            assert dynamic_class.local == 99
+        with SetClassAttribute(dynamic_class, dynamic=99):
+            assert dynamic_class.dynamic == 99
 
-        assert dynamic_class.local == 42
+        assert dynamic_class.dynamic == 42
+
+    assert dynamic_class.dynamic == None
 
     assert dynamic_class.local == None
